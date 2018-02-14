628c0b3c463ed6a472d95bcba734c034911d3776
Artem Vozhzhov <vojjov.artem@ya.ru>
2018-02-14 13:57:44 +0300

    added portfolio manage functionality

---
+++ src/Entity/Portfolio.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Timestamp\HasTimestampsInterface;
+use DateTime;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PortfolioRepository")
+ */
+class Portfolio implements HasTimestampsInterface
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @var \App\Entity\User
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(max=255)
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \App\Entity\User
+     */
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param \App\Entity\User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $timestamp
+     */
+    public function setCreatedAt(DateTime $timestamp): void
+    {
+        $this->createdAt = $timestamp;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $timestamp
+     */
+    public function setUpdatedAt(DateTime $timestamp): void
+    {
+        $this->updatedAt = $timestamp;
+    }
+}
---
+++ src/Form/Portfolio/PortfolioCreateType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form\Portfolio;
+
+use App\Entity\Portfolio;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PortfolioCreateType extends AbstractType
+{
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class, ['empty_data' => '']);
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => Portfolio::class]);
+    }
+
+}
