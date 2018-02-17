45b1fbbe397d2d97b89ad6398d0e2ab80c5c8ae9
Artem Vozhzhov <vojjov.artem@ya.ru>
2018-02-17 16:07:34 +0300

    removed dashboard page, added redirect to portfolio index page from dasboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends Controller
 {
     /**
      * @Route("/", name="dashboard")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('dashboard.html.twig');
+        return $this->redirectToRoute('portfolio_index');
     }
 }
